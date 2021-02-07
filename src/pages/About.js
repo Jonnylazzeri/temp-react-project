a5a54d47e6dc7e16599d22818d64d90674c558d4
Jonnylazzeri <jonnylazzeri94@gmail.com>
2021-02-07 04:22:45 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quam
+        repudiandae, porro voluptas enim labore amet sunt neque corrupti
+        corporis rerum, dolorem esse ut libero fuga magni soluta culpa
+        consequatur blanditiis iure maxime. Enim molestiae maiores minus
+        voluptas porro facere.
+      </p>
+    </section>
+  );
+};
+
+export default About;
